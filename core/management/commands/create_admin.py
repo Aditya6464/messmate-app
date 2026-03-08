44f36fec6098e310faf9d85e92bd497e4b0d77b2
adipatil6464 <91333594+adipatil6464@users.noreply.github.com>
2026-03-08 14:22:53 +0530

    Auto-create superuser on deployment

---
+++ core/management/commands/create_admin.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = 'Creates a superuser from environment variables'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser: {username}'))
+        else:
+            self.stdout.write(self.style.SUCCESS(f'Superuser {username} already exists.'))
